d2e303fa35ef871fa4c38328824c46f4fc7ecf65
Timkahi <91555197+Timkahi@users.noreply.github.com>
2023-03-09 11:17:30 +0300

    fix: start new type of bot

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import axios from 'axios'
 import { config } from 'dotenv'
 import express from 'express'
+config()
+const app = express()
+const JOKE_API = 'https://v2.jokeapi.dev/joke/Programming?type=single'
+const TELEGRAM_URI = `https://api.telegram.org/bot${botToken.env.TELEGRAM_API_TOKEN}/sendMessage`
+
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.post('/new-message', async (req, res) => {
+    // ...
+  })
